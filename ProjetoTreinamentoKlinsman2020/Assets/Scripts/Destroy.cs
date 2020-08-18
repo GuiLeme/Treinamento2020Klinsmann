28820bfe2871238449c12638cb9b6d054bed4d44
GuiLeme <69865537+GuiLeme@users.noreply.github.com>
2020-08-18 15:12:47 -0300

    Destruição
    Eu consegui destruir o objeto de cima

---
+++ ProjetoTreinamentoKlinsman2020/Assets/Scripts/Destroy.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour
+{
+    public GameObject toDestroy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(toDestroy);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
