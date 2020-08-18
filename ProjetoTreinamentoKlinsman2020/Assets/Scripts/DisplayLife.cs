3a4b6e2e630f3cfd9ac5197dd889f4ce6ed5193d
GuiLeme <69865537+GuiLeme@users.noreply.github.com>
2020-08-18 15:35:33 -0300

    Vida display
    Incremento da vida do personagem sendo mostrada

---
+++ ProjetoTreinamentoKlinsman2020/Assets/Scripts/DisplayLife.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayLife : MonoBehaviour
+{
+    public Text vidaUI;
+    public int vidaPersonagem;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        vidaUI.text = "Vida: " + vidaPersonagem.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
