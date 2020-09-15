ae4cc6efdefa74414a65c30fe373aa96ed72e6a8
GuiLeme <69865537+GuiLeme@users.noreply.github.com>
2020-09-15 00:10:34 -0300

    tentei fazer o inimigo virar

--- ProjetoTreinamentoKlinsman2020/Assets/Enemy_Run.cs
+++ ProjetoTreinamentoKlinsman2020/Assets/Enemy_Run.cs
@@ -4,21 +4,25 @@ using UnityEngine;
 
 public class Enemy_Run : StateMachineBehaviour
 {
-    public float speed = 7f;
+    public float speed = 10f;
 
     Transform player;
     Rigidbody2D rb;
+    EnemyLookAtPlayer enemy;
 
      //OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
         rb = animator.GetComponent<Rigidbody2D>();
+        enemy = animator.GetComponent<EnemyLookAtPlayer>();
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+        enemy.LookAtPlayer();
+
         Vector2 target = new Vector2(player.position.x, rb.position.y);
         Vector2 newPos = Vector2.MoveTowards(rb.position, target, speed * Time.fixedDeltaTime);
         rb.MovePosition(newPos);
